2e54cee9fcd6622be64a4621a8762666a1ca8033
hnmr <hnmr293@gmail.com>
2026-04-05 05:22:34 +0900

    improve build error message

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+import sys
+import sysconfig
 from setuptools import setup, Extension
+
+header_dir = sysconfig.get_path("include")
+if header_dir is None or not __import__("os").path.isfile(__import__("os").path.join(header_dir, "Python.h")):
+    v = f"{sys.version_info.major}.{sys.version_info.minor}"
+    sys.exit(
+        f"error: Python.h not found for Python {v}.\n"
+        f"Install the development headers, e.g.:  sudo apt install python{v}-dev"
+    )
 
 setup(
     ext_modules=[
